6c3bf572d4f289d2b1a3ca90d3caf2efdad09023
DEEP-32 <deepanshusingh@backstagepass.co.in>
2024-04-07 20:01:17 +0530

    [ADD] simplex noise

--- Assets/Scripts/Jobs/Noise.Lattice.cs
+++ Assets/Scripts/Jobs/Noise.Lattice.cs
@@ -69,7 +69,7 @@ public static partial class Noise {
             LatticeSpan4 x = default(L).GetLatticeSpan4(positions.c0,frequency);
 
             var g = default(G);
-            return g.EvaluateAfterInterpolation(lerp(
+            return g.EvaluateCombined(lerp(
                 g.Evaluate(hash.Eat(x.p0),x.g0), 
                 g.Evaluate(hash.Eat(x.p1),x.g1), 
                 x.t
@@ -86,7 +86,7 @@ public static partial class Noise {
             SmallXXHash4 h0 = hash.Eat(x.p0), h1 = hash.Eat(x.p1);
 
             var g = default(G);
-            return g.EvaluateAfterInterpolation(lerp(
+            return g.EvaluateCombined(lerp(
                 lerp(
                     g.Evaluate(h0.Eat(z.p0),x.g0,x.g1), 
                     g.Evaluate(h0.Eat(z.p1),x.g0,x.g1), 
@@ -118,7 +118,7 @@ public static partial class Noise {
 
 
             var g = default(G);
-            return g.EvaluateAfterInterpolation(lerp(
+            return g.EvaluateCombined(lerp(
                 lerp(
                     lerp(
                         g.Evaluate(h00.Eat(z.p0), x.g0, y.g0, z.g0),
--- Assets/Scripts/SmallXXHash.cs
+++ Assets/Scripts/SmallXXHash.cs
@@ -94,6 +94,8 @@ public readonly struct SmallXXHash4 {
         avalanche ^= avalanche >> 16;
         return avalanche;
     }
+    public static SmallXXHash4 Select(SmallXXHash4 a, SmallXXHash4 b, bool4 c) =>
+        math.select(a.accumulator, b.accumulator, c);
     public uint4 GetBits(int count, int shift) =>
         ((uint4)this >> shift) & (uint)((1 << count) - 1);
 
